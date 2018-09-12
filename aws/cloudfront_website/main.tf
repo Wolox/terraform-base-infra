9e37d8f6ed65e40c92ee884ffa3f4c6cf49c62d5
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2018-09-12 09:56:12 -0300

    Added response status

--- aws/cloudfront_website/main.tf
+++ aws/cloudfront_website/main.tf
@@ -26,7 +26,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     custom_error_response {
       error_code            = 404
-      response_page_path    = "/"
+      response_page_path    = "/index.html"
+      response_code         = 200
     }
   }
 
