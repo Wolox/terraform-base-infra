a06d30c0b8897c41e67663f5342d82bb20f9cd03
Martín Montenegro <Martum@users.noreply.github.com>
2018-09-13 16:24:14 -0300

    Fix NACL port range for outbound traffic (#12)

--- aws/vpc/dbs_subnet.tf
+++ aws/vpc/dbs_subnet.tf
@@ -8,27 +8,26 @@ resource "aws_network_acl" "private_dbs_subnet" {
   }
 }
 
-
 resource "aws_network_acl_rule" "private_dbs_public_subnet_ingress" {
-  network_acl_id  = "${aws_network_acl.private_dbs_subnet.id}"
-  egress          = false
-  count           = "${length(var.azs)}"
-  protocol        = "tcp"
-  rule_number     = "11${count.index}"
-  rule_action     = "allow"
-  cidr_block      = "${aws_subnet.public.*.cidr_block[count.index]}"
-  from_port       = "${var.db_port}"
-  to_port         = "${var.db_port}"
+  network_acl_id = "${aws_network_acl.private_dbs_subnet.id}"
+  egress         = false
+  count          = "${length(var.azs)}"
+  protocol       = "tcp"
+  rule_number    = "11${count.index}"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.public.*.cidr_block[count.index]}"
+  from_port      = "${var.db_port}"
+  to_port        = "${var.db_port}"
 }
 
 resource "aws_network_acl_rule" "private_dbs_public_return_traffic" {
-  network_acl_id  = "${aws_network_acl.private_dbs_subnet.id}"
-  egress          = true
-  count           = "${length(var.azs)}"
-  protocol        = "tcp"
-  rule_number     = "13${count.index}"
-  rule_action     = "allow"
-  cidr_block      = "${aws_subnet.public.*.cidr_block[count.index]}"
-  from_port       = 32768
-  to_port         = 65535
+  network_acl_id = "${aws_network_acl.private_dbs_subnet.id}"
+  egress         = true
+  count          = "${length(var.azs)}"
+  protocol       = "tcp"
+  rule_number    = "13${count.index}"
+  rule_action    = "allow"
+  cidr_block     = "${aws_subnet.public.*.cidr_block[count.index]}"
+  from_port      = 1024
+  to_port        = 65535
 }
