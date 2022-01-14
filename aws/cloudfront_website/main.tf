4b546a46093901605285c0a443ea3b9f23bf29af
bvolpini <volpinibruno@gmail.com>
2022-01-14 11:45:45 -0300

    fixing TLS in cloudfront_static_website module

--- aws/cloudfront_website/main.tf
+++ aws/cloudfront_website/main.tf
@@ -61,7 +61,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     cloudfront_default_certificate = "${!local.custom_certificate}"
 
     acm_certificate_arn      = "${local.custom_certificate ? element(concat(data.aws_acm_certificate.c.*.arn, list("")), 0) : ""}"
-    minimum_protocol_version = "${local.custom_certificate ? "TLSv1.1_2016" : "TLSv1"}"
+    #minimum_protocol_version = "${local.custom_certificate ? "TLSv1.1_2016" : "TLSv1"}"
+    minimum_protocol_version = "${local.custom_certificate ? "TLSv1.2_2019" : "TLSv1.2"}"
     ssl_support_method       = "sni-only"
   }
 
